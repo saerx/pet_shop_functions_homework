a910907201803211d502c125be4c0d1e3688901c
saerx <saerlaith.robyn@gmail.com>
2020-11-07 22:00:33 +0000

    added get_pets_by_breed function

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,3 +16,10 @@ def increase_pets_sold(shop, pet_num):
 
 def get_stock_count(shop):
     return len(shop["pets"])
+
+def get_pets_by_breed(shop, breed):
+    breed_matches = []
+    for pet in shop["pets"]:
+       if pet["breed"] == breed:
+           breed_matches.append(pet)
+    return breed_matches
